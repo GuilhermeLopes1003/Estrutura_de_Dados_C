740ebc4678106ec504f9cde1b555f40495a0e952
unknown <guilopez2004@gmail.com>
2026-09-17 16:00:08 -0300

    Criação do Menu e funçar Inserçao

--- Biblioteca_Projeto1.h
+++ Biblioteca_Projeto1.h
@@ -1,19 +1,18 @@
-#ifndef Biblioteca_Projeto1_H
-#define Biblioteca_Projeto1_H
+#ifndef BIBLIOTECA_PROJETO1_H
+#define BIBLIOTECA_PROJETO1_H
 
-
-typedef struct Manuntencaoo
+typedef struct manutencao
 {
     int codigoSolicitacao;
     char codigoEquipamento[7];
     char nomeEquipamento[21];
     int prioridade;
     int periodo;
-} Manuntencao;
+} Manutencao;
 
 typedef struct no
 {
-    manuntencao info;
+    Manutencao info;
     struct no *prox;
 } No;
 
@@ -21,3 +20,91 @@ typedef struct lista
 {
     No *inicio;
 } Lista;
+
+
+
+
+
+Lista* InicializaLista()
+{
+    return NULL;
+}
+
+
+Lista* CriaLista()
+{
+    Lista *aux;
+    aux = (Lista*)malloc(sizeof(Lista));
+    if (aux == NULL)
+    {
+        printf("Erro: memoria insuficiente.\n");
+    }
+    else
+    {
+        aux->inicio = NULL;
+    }
+    return aux;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+void inserir(Lista *L, Manutencao dados)
+{
+    No *novo = (No *) malloc(sizeof(No));
+
+    if (novo == NULL)
+    {
+        printf("Erro: Nao foi possivel fazer a alocaçao de memoria!.\n");
+    }
+    else
+    {
+        novo->info = dados;
+        novo->prox = NULL;
+
+        No *atual = L->inicio;
+        No *anterior = NULL;
+        int duplicado = 0;
+
+        while (atual != NULL && atual->info.codigoSolicitacao < dados.codigoSolicitacao) 
+        {
+            anterior = atual;
+            atual = atual->prox;
+        }
+
+        if (atual != NULL && atual->info.codigoSolicitacao == dados.codigoSolicitacao) // checagem inserçao duplicada
+        {
+            duplicado = 1;
+        }
+
+        if (duplicado == 1)
+        {
+            printf("Erro: codigo de solicitacao %d ja existe.\n", dados.codigoSolicitacao);
+            free(novo);
+        }
+        else
+        {
+            if (anterior == NULL)  // condição inserir no inicio da lista
+            {
+                novo->prox = L->inicio;
+                L->inicio = novo;
+            }
+            else                          // condição inserir no meio ou no fim da lista
+            {
+                novo->prox = atual;
+                anterior->prox = novo;
+            }
+        }
+    }
+}
+
+
+#endif
---
+++ Gerenciador_Manuntencao.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Biblioteca_Projeto1.h"
+
+int main()
+{
+    Lista *L;
+    L = InicializaLista();
+    L = CriaLista();
+
+    int opcao;
+    Manutencao dados;
+
+    do
+    {
+        printf("\n===== MENU =====\n");
+        printf("1 - Inserir uma Solicitacao de Manutencao no Sistema\n");
+        printf("2 - Remover uma Solicitacao no Sistema\n");
+        printf("3 - Consultar uma Solicitacao no Sistema\n");
+        printf("4 - Alterar prioridade e/ou periodo no Sistema\n");
+        printf("5 - Exibir ordem de realizacao da manutencao no Sistema\n");
+        printf("6 - Exibir todas as solicitacoes no Sistema\n");
+        printf("0 - Finalizar/Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+            case 1:
+
+                printf("Codigo da solicitacao: ");
+                scanf("%d", &dados.codigoSolicitacao);
+
+                printf("Codigo do equipamento: ");
+                scanf("%s", dados.codigoEquipamento);
+
+                printf("Nome do equipamento: ");
+                scanf(" %[^\n]", dados.nomeEquipamento);
+
+                printf("Prioridade (1-Alta, 2-Media, 3-Baixa): ");
+                scanf("%d", &dados.prioridade);
+
+                printf("Periodo (dias): ");
+                scanf("%d", &dados.periodo);
+
+                inserir(L, dados);
+                break;
+
+            case 2:
+                // TODO: chamar remover(...)
+                printf("Opcao Remover ainda nao implementada.\n");
+                break;
+
+            case 3:
+                // TODO: chamar consultar(...)
+                printf("Opcao Consultar ainda nao implementada.\n");
+                break;
+
+            case 4:
+                // TODO: chamar alterar(...)
+                printf("Opcao Alterar ainda nao implementada.\n");
+                break;
+
+            case 5:
+                // TODO: chamar exibirOrdem(...)
+                printf("Opcao Exibir Ordem ainda nao implementada.\n");
+                break;
+
+            case 6:
+                // TODO: chamar exibirTodas(...)
+                printf("Opcao Exibir Todas ainda nao implementada.\n");
+                break;
+
+            case 0:
+                printf("Encerrando o programa...\n");
+                // TODO: chamar funcao de liberar memoria antes de sair
+                break;
+
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+                break;
+        }
+
+    } while (opcao != 0);
+
+    return 0;
+}
